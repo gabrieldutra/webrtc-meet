7a55c9c1a5c5748f839f1880e59673a547595d78
Gabriel Dutra <nesk.frz@gmail.com>
2021-09-05 15:09:29 -0300

    Add skip to useLocalMediaStream

--- lib/useLocalMediaStream.ts
+++ lib/useLocalMediaStream.ts
@@ -1,6 +1,19 @@
+import { merge } from "lodash";
 import { useEffect, useState } from "react";
 
-export function useLocalMediaStream() {
+export interface UseLocalMediaStreamOptions {
+  skip: boolean;
+}
+
+const DEFAULT_OPTIONS = {
+  skip: false,
+};
+
+export function useLocalMediaStream(
+  options?: Partial<UseLocalMediaStreamOptions>
+) {
+  const { skip } = merge({}, DEFAULT_OPTIONS, options);
+
   const [localMediaStream, setLocalMediaStream] = useState<MediaStream>();
 
   useEffect(() => {
@@ -14,14 +27,15 @@ export function useLocalMediaStream() {
       if (!cancel) setLocalMediaStream(localMedia);
     };
 
-    loadLocalMedia();
+    if (!skip) loadLocalMedia();
+
     return () => {
       if (localMedia) {
         localMedia.getTracks().forEach((track) => track.stop());
       }
       cancel = true;
     };
-  }, []);
+  }, [skip]);
 
   return localMediaStream;
 }
